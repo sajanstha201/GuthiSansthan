3c338756af23c24ade03940ebe1a13316da41942
unknown <bikashbhandari600@gmail.com>
2024-07-15 17:12:54 +0545

    added by bikash

--- src/components/ServicePage/ServicePage.js
+++ src/components/ServicePage/ServicePage.js
@@ -1,10 +1,25 @@
 import { InstanceService } from "./InstanceService"
 import img from '../../media/AboutUsPage/Janaki_temple_in_Nepal.jpeg'
+import bgImage from '../../media/ServicePage/architecture.png'
+
 export const ServicePage=()=>{
     return(
         <>
-        <h2>This is Service Page</h2>
-        <InstanceService image={img} link={'sajan'} name={'sajan service'} />
+        <div style={{backgroundImage:`url(${bgImage})`}} className="fixed w-screen h-screen bg-cover bg-center top-0 -z-10">
+
+        </div>
+        <div  className="fixed bg-zinc-800/25 bg-center top-0 w-screen h-screen">
+
+              </div>
+        
+        <div className='w-full h-full bg-cover bg-center py-10 lg:h-screen flex justify-start items-center gap-10 px-14 flex-wrap' >
+
+        <InstanceService image={img} link={'sajan'} name={'sajan service'} des={'This setup creates a navigation bar that changes its background color and other styles when the user scrolls down the page. You can customize the styles andtup creates a navigation bar that changes its background color and other '}/>
+        <InstanceService image={img} link={'sajan'} name={'sajan service'} des={'This setup creates a navigation bar that changes its background color and other styles when the user scrolls down the page. You can customize the styles andtup creates a navigation bar that changes its background color and other '}/>
+        <InstanceService image={img} link={'sajan'} name={'sajan service'} des={'This setup creates a navigation bar that changes its background color and other styles when the user scrolls down the page. You can customize the styles andtup creates a navigation bar that changes its background color and other '}/>
+        <InstanceService image={img} link={'sajan'} name={'sajan service'} des={'This setup creates a navigation bar that changes its background color and other styles when the user scrolls down the page. You can customize the styles andtup creates a navigation bar that changes its background color and other '}/>
+        </div>
+    
         </>
     )
 }

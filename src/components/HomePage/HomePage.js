f22f0d1f0a49b0c7a3430a35d4be5b2468797041
sajan shrestha <sajanshrestha@sajan-shrestha-2.local>
2024-07-15 23:05:30 +0545

    added the bottom popout

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -4,13 +4,13 @@ import { useTranslation } from "react-i18next";
 import { NepalFlagSlider } from "./NepalFlagSlider";
 import { Link, useLocation } from "react-router-dom";
 import homeBgVideo from '../../media/HomePage/My Movie.mp4';
-
+import { HomeFooter } from "./HomeFooter";
 export const HomePage = () => {
   const { t } = useTranslation();
   const isMobile = useMediaQuery('(max-width:800px)');
   const location=useLocation()
   return (
-    <div className="relative h-screen">
+    <div className="relative h-[90vh]">
       {/* {location.pathname!=='/'&&<video autoPlay loop muted className="video-background absolute inset-0 w-full h-full object-cover">
         <source src={homeBgVideo} type="video/mp4" />
         Your browser does not support the video tag.
@@ -18,7 +18,7 @@ export const HomePage = () => {
       <div className="absolute top-0 left-0 w-full h-full bg-black opacity-20"></div>
       } */}
       
-      <div className="flex flex-col items-center justify-center h-full relative">
+      <div className="flex flex-col items-center justify-center h-full relative overflow-hidden">
         <NepalFlagSlider />
         <div className={`flex ${isMobile ? 'flex-col' : 'flex-row'}  gap-4 px-5 w-full`}>
           <Link
@@ -30,6 +30,7 @@ export const HomePage = () => {
             {t('sign-in')}
           </Link>
         </div>
+        <HomeFooter/>
       </div>
     </div>
   );

55e4506aa7b7627cc5b7a81ff56e7218fc496e42
sajan shrestha <sajanshrestha@sajan-shrestha-2.local>
2024-07-17 23:17:40 +0545

    done with the language selection section

--- src/components/Navbar/HeaderTop.js
+++ src/components/Navbar/HeaderTop.js
@@ -46,15 +46,15 @@ export const HeaderTop = () => {
 
     return (
         <div className={`${isMobile ? 'h-[80px]' : 'flex-row px-20 h-[100px]'} flex w-screen justify-between items-center p-2`}>
-            <Link to='/' className={`${isMobile ? 'flex-col justify-start items-center w-[60%]' : 'flex-row w-[30%] items-center'} h-full flex-row flex`}>
+            <Link to='/' className={`${isMobile ? 'flex-col justify-start items-center w-[50%]' : 'flex-row w-[30%] items-center'} h-full flex-row flex`}>
                 <img className={`${isMobile ? 'h-[40px]' : 'h-[80px]'} backdrop-blur-md`} src={nepalLogo} />
                 <img className={`${isMobile ? 'h-[40px] pr-4' : 'h-[80px] pr-10'} backdrop-blur-md bg-yellow-50 rounded-full shadow-lg`} src={guthiLogo} />
             </Link>
             {loc.pathname !== '/' && !isMobile && <div className={`${isMobile ? 'h-[100px]' : ''} w-[40%] flex items-center justify-center`}>
                 <HeaderButtom />
             </div>}
-            <div className={`${isMobile ? 'gap-2 w-[40%]' : 'gap-7 w-[20%]'} relative flex-row flex h-full items-center justify-start px-2`}>
-                <div ref={divRef} className={`${isMobile ? 'gap-2 ' : 'gap-7 '} w-[60%] p-2 relative flex-row flex h-full justify-start  `}>
+            <div className={`${isMobile ? 'gap-2 w-[50%]' : 'gap-7 w-[30%]'} relative flex-row flex h-full items-center justify-start px-2`}>
+                <div ref={divRef} className={`${isMobile ? 'gap-2 ' : 'gap-7 '} w-[40%]  relative flex-row flex h-full items-center justify-center  `}>
                     <div className={`${isMobile ? 'h-[30px] w-[30px]':'h-[60px] w-[60px]'} relative cursor-pointer transition-all  items-center flex justify-center   `}
                             onClick={() => { setLanguageOptionHidden(!languageOptionHidden); }}>
                                 <div className="z-30 bg-red-700 rounded-full overflow-hidden h-full w-full items-center flex justify-center">
@@ -64,7 +64,7 @@ export const HeaderTop = () => {
                                 {selectLanguage === 'mithila' && <img src={mithilaFlag}  />}
                                 </div>
 
-                            <div className={`${languageOptionHidden ? 'w-[200%] left-[10%]' : 'opacity-100 w-[400%] left-[-15px] '}   absolute h-full flex flex-row  items-start z-20 transition-all duration-500 rounded-full`}>
+                            <div className={`${languageOptionHidden ? 'w-[200%] left-[5%]' : 'opacity-100 w-[400%] left-[-30px] '}   absolute h-full flex flex-row  items-start z-20 transition-all duration-500 rounded-full`}>
                                 {['nepali','newari','english','mithila'].filter(lng=>lng!==selectLanguage).map(
                                     (key,index)=>(
                                     <Language name={key} handleLanguageClick={handleLanguageClick} img={flagInfo[key]} no={index}></Language>))}

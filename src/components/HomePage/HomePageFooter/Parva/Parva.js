824a6bec44286985e67b45b4c609f61f0829ebfd
sajan shrestha <sajanshrestha@sajan-shrestha-2.local>
2024-07-17 16:31:14 +0545

    added

--- src/components/HomePage/HomePageFooter/Parva/Parva.js
+++ src/components/HomePage/HomePageFooter/Parva/Parva.js
@@ -6,7 +6,7 @@ export const Parva=()=>{
         <div className="w-full h-full pb-[100px] flex flex-col ">
         <h1 className="text-white z-40 text-[60px]">Parva</h1>
         <div className="flex w-full h-full items-center justify-center overflow-auto">
-            <div className="h-full w-[95%] flex  flex-wrap items-center justify-center gap-7">
+            <div className=" w-[95%] flex  flex-wrap items-center justify-center gap-7">
                     <ParvaInstance img={img} name={'Dashain'} detail={'lsdkfjsalkdf'}/>
                     <ParvaInstance img={img} name={'Dashain'} detail={'lsdkfjsalkdf'}/>
                     <ParvaInstance img={img} name={'Dashain'} detail={'lsdkfjsalkdf'}/>

33e2def51811899c24f010bfdddcf78ff2484ca0
sajan shrestha <sajanshrestha@sajan-shrestha-2.local>
2024-07-19 13:55:10 +0545

    done

--- src/components/Navbar/Language.js
+++ src/components/Navbar/Language.js
@@ -5,10 +5,10 @@ export const Language=({name,img,handleLanguageClick,no})=>{
     const isMobile=useMediaQuery('(max-width:1000px)')
     return(
         <>
-            <div className={`${selectLanguage===name?'hidden ':`${isMobile ? 'h-[15px] w-[15px]' : 'h-[40px] w-[40px]'}`}  shadow-lg cursor-pointer  items-center flex justify-centeroverflow-hidden`}
+            <div className={`${selectLanguage===name?'hidden ':`${isMobile ? 'h-[15px]' : 'h-[50px] '}`} shadow-lg cursor-pointer  items-center flex justify-centeroverflow-hidden`}
                 onClick={() => { handleLanguageClick(name) }}
                 style={{right:`${no/5*100}%`}}>
-                <img src={img} className={`${selectLanguage===name?'hidden w-0':``}`} />
+                <img src={img} className={`${selectLanguage===name?'hidden ':``} h-full `} />
             </div>
         </>
     )

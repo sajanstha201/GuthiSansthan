96595a437565d298cd543c5ec7f3d7249f06cfd7
sajan shrestha <sajanshrestha@sajan-shrestha-2.local>
2024-07-25 22:07:54 +0530

    done with all the static folders

--- src/components/Articles/ArticleMainSection.js
+++ src/components/Articles/ArticleMainSection.js
@@ -1,18 +1,31 @@
 import { Articles } from "./ArticleSection/Articles"
 import { Notices } from "./NoticeSection/Notices"
-import bg from '../../media/Article/pasupatibg.jpg'
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useMediaQuery } from "@mui/material";
-
-
+import {useDispatch, useSelector} from 'react-redux'
+import axios from "axios"
+import { setArticlePageWholeDetail, setBgImg } from "../../state/ArticlePageSlice";
+import {addLanguage, fetchImageToURL} from '../ReuseableFunctions'
 export const ArticleMainSection=()=>{
     const isMobile = useMediaQuery('(max-width:800px)');  
     const [isArticle,setArtical]=useState(true)
+    const articlePageDetail=useSelector(state=>state.articlePageDetail)
+    const baseUrl=useSelector(state=>state.baseUrl).backend
+    const dispatch=useDispatch()
     
+    useEffect(()=>{
+        const fetchData=async()=>{
+            const response=await axios.get(baseUrl+articlePageDetail.url)
+            dispatch(setArticlePageWholeDetail(response.data.components))
+            dispatch(setBgImg(await fetchImageToURL(baseUrl+response.data.components['bg-img'].image)))
+            addLanguage({key:'article-page-heading',lngs:response.data.components['bg-img'].text})
+        }
+        if(!articlePageDetail.isFetched) fetchData()
+    })
 
     return(
         <>
-         <div className="bg-cover bg-center h-screen w-full fixed -z-50 top-0"style={{backgroundImage:`url(${bg})`}}></div>
+         <div className="bg-cover bg-center h-screen w-full fixed -z-50 top-0"style={{backgroundImage:`url(${articlePageDetail['bg-img'].imgSrc})`}}></div>
          <div className="bg-cover bg-center h-screen w-full fixed -z-10 top-0 bg-black/40"></div>
          {isMobile ? <div className="w-full">
                    <div className="w-full py-2 flex justify-start bg-gray-400/80  gap-4 pl-16">
---
+++ src/state/ContactUsPageSlice.js
@@ -0,0 +1,44 @@
+import { createSlice } from "@reduxjs/toolkit";
+export const ContactUsPageSlice=createSlice({
+    name:"contactUsPage",
+    initialState:{
+        isFetched:false,
+        url:'api/pages/contact-us/',
+        "bg-img":{imgSrc:"",id:"",actualImgSrc:"",isFetched:false},
+        "details":{},
+        "extra-image-1":{imgSrc:"",id:"",actualImgSrc:"",isFetched:false},
+        "extra-image-2":{imgSrc:"",id:"",actualImgSrc:"",isFetched:false}
+    },
+    reducers:{
+        setContactUsPageWholeDetail:(state,action)=>{
+            state.isFetched=true
+            state.details=action.payload
+        },
+        setExtraImage1:(state,action)=>{
+            state["extra-image-1"]={
+                isFetched:true,
+                imgSrc:action.payload,
+                actualImgSrc:action.payload,
+                id:state.details["extra-image-1"].id
+            }
+        },
+        setExtraImage2:(state,action)=>{
+            state["extra-image-2"]={
+                isFetched:true,
+                imgSrc:action.payload,
+                actualImgSrc:action.payload,
+                id:state.details["extra-image-2"].id
+            }
+        },
+        setBgImg:(state,action)=>{
+            state['bg-img']={
+                isFetched:true,
+                imgSrc:action.payload,
+                actualImgSrc:action.payload,
+                id:state.details["bg-image"].id
+            }
+        }
+    }
+})
+export default ContactUsPageSlice.reducer
+export const {setContactUsPageWholeDetail,setExtraImage1,setExtraImage2,setBgImg}=ContactUsPageSlice.actions

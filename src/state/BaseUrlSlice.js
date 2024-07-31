141d4a6b174d5de63ea51e29c7ce5650af1782d2
sajan shrestha <sajanshrestha@sajan-shrestha-2.local>
2024-07-31 09:22:12 +0530

    changed teh api calling url

--- src/state/BaseUrlSlice.js
+++ src/state/BaseUrlSlice.js
@@ -5,10 +5,11 @@ export const BaseUrlSlice=createSlice({
     initialState:{
         // 'backend':'http://192.168.1.140:8000/',
         //  'backend':'http://192.168.1.65:8000/',
-         'backend':'http://guthi.pythonanywhere.com/',
+         'backend':'https://guthi.pythonanywhere.com/',
     //    'backend':' http://127.0.0.1:8000/',
         // 'backend':'http://192.168.1.65:8000/',
-        'frontend':'http://localhost:3000/'
+        'frontend':'http://localhost:3000/',
+        // 'frontend':'https://sajanstha201.github.io/GuthiSansthan/'
     },
     reducers:{
     }

0a6b3af0053fb8e8dda650e50d8cc3cc0ebb01f5
sajan shrestha <sajanshrestha@sajan-shrestha-2.local>
2024-08-06 18:09:35 +0530

    changed teh base url

--- src/state/BaseUrlSlice.js
+++ src/state/BaseUrlSlice.js
@@ -5,9 +5,9 @@ export const BaseUrlSlice=createSlice({
     initialState:{
         // 'backend':'http://192.168.1.140:8000/',
         //  'backend':'http://20.198.220.208:8001/',
-        //  'backend':'https://guthi.pythonanywhere.com/',
+         'backend':'https://guthi.pythonanywhere.com/',
 
-       'backend':' http://127.0.0.1:8000/',
+    //    'backend':' http://127.0.0.1:8000/',
         // 'backend':'http://192.168.1.65:8000/',
         'frontend':'http://localhost:3000/',
         // 'frontend':'https://sajanstha201.github.io/GuthiSansthan/'
